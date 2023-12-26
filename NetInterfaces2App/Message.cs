59d27a6b36e790027d30ecc93a2856d314e35dfc
maxefimov <efimov.top.academy@gmail.com>
2023-12-26 20:10:22 +0300

    Добавьте файлы проекта.

---
+++ NetInterfaces2App/Message.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetInterfaces2App
+{
+    interface IMessage
+    {
+        string Text { set; get; }
+    }
+
+    interface ISending
+    {
+        void SendMessage();
+    }
+
+    class Message : IMessage, ISending
+    {
+        public string Text { get; set; }
+
+        public Message(string text)
+        {
+            Text = text;
+        }
+
+        public void SendMessage()
+        {
+            Console.WriteLine($"Send message: {Text}");
+        }
+    }
+
+    class Messanger<T> where T : ISending, IMessage
+    {
+        public void Send(T message)
+        {
+            message.SendMessage();
+        }
+    }
+}
---
+++ NetInterfaces2App/Program.cs
@@ -0,0 +1,65 @@
+﻿using NetInterfaces2App;
+using System.Threading.Channels;
+
+IMovable movable = new Transport();
+movable.Move();
+movable = new Car();
+movable.Move();
+
+Transport transport = new Transport();
+transport.Move();
+transport = new Car();
+transport.Move();
+
+Car car = new Car();
+car.Move();
+
+Animal animal = new Animal();
+IMovable movableAnimal = animal;
+(animal as IMovable).Move();
+movableAnimal.Move();
+
+IGroundMovable movableGroundAnimal = animal;
+(animal as IGroundMovable).Move();
+movableGroundAnimal.Move();
+
+//
+Message message = new("Hello world");
+Messanger<Message> messanger = new Messanger<Message>();
+messanger.Send(message);
+//
+
+
+interface IMovable
+{
+    void Move() => Console.WriteLine("Object move!");
+}
+
+interface IGroundMovable : IMovable
+{
+    new void Move() => Console.WriteLine("Object move on ground!");
+}
+
+class Transport : IMovable
+{
+    public void Move() => Console.WriteLine("Transport move!");
+}
+
+class Car : Transport
+{
+    public new void Move()
+    {
+        Console.WriteLine("Car move!");
+    }
+}
+
+class Animal : IGroundMovable
+{
+    //public void Move()
+    
+}
+
+
+
+
+
